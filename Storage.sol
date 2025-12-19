63abca7a86d07f1ddd54a6da329ec1a37087a232
PranshuMishra2004 <pranshu952876@gmail.com>
2025-12-20 03:01:07 +0530

    Add files via upload

---
+++ Storage.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract Strorage1{
+
+    uint256 Number;     // let a number variable default to 0
+
+    function Store (uint256 numberToSave) view  public{   //fucntion to store Number
+        numberToSave=Number;
+    }
+
+    function retrive() public view returns(uint256) {    // fucntion to retrieve Number
+        return Number;
+    }
+
+    struct Person {         // struct an array
+        uint256 age;
+        string Name;
+    }
+    Person[] public people;   // create an dynamic array
+
+    mapping (string => uint256) public nameToNumber;  // mapping the stored number to a perticular string
+
+    function addPeople(uint256 number,string memory name) public {   // add name and number to array
+        people.push(Person(number, name));
+        nameToNumber[name]= number;    // mapping the number passed to the passed string
+    }
+
+}
